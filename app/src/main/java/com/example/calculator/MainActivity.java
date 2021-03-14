d61219489a2b3fee015978b6fbd946026576b0e2
lingislong <silverhorse.shay@gmail.com>
2021-03-14 17:00:01 +0200

    have fun with android java
    Signed-off-by: lingislong <silverhorse.shay@gmail.com>

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,61 +1,78 @@
 package com.example.calculator;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    int soreNum1, soreNum2;
+    int Num1, Num2;
     char op;
-    TextView textViewObj;
+    TextView textViewResult;
+    //SharedPreferences.Editor prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewObj = findViewById(R.id.textViewResult);
-        textViewObj.setText("" + 0);
-        soreNum = 0;
+        textViewResult = findViewById(R.id.textViewResult);
+
+        textViewResult.setText("" + 0);
+        Num1 = 0;
+        Num2 = 0;
+
+        Toast.makeText(this, "Go", Toast.LENGTH_SHORT).show();
 
     }
 
-
+    //soreNum1
     public void funcNumber(View view) {
 
+        //String result = textViewResult.getText().toString();
+        Integer.parseInt((String) ((Button) view).getText());
+        Num1 = Integer.parseInt((String) textViewResult.getText()) * 10;
 
-        soreNum1 = Integer.parseInt((String) textViewObj.getText()) * 10;
+        //Toast.makeText(this, ""+result, Toast.LENGTH_SHORT).show();
 
-        soreNum1 += Integer.parseInt((String) ((Button) view).getText());
+        if (Num1 == 0)
+            textViewResult.setText("");
 
-        textViewObj.setText("" + soreNum1);
+        textViewResult.append("" + Num1);
 
     }
 
     public void funcClearAll(View view) {
-        soreNum1 = 0;
-        textViewObj.setText("" + 0);
+        Num1 = 0;
+        Num2 = 0;
+        textViewResult.setText("" + 0);
     }
 
     public void funcEq(View view) {
 
-
+        //     switch (op) {
+//            case '+':
+//                soreNum1 += soreNum2;
+//                break;
+//            default:
+//                throw new IllegalStateException("Unexpected value: " + op);
+//        }
+//        textViewObj.setText("" + soreNum1);
     }
 
     public void funcOP(View view) {
-        op = (char) Integer.parseInt((String) ((Button) view).getText());
-        textViewObj.setText("" + 0);
-        switch (op) {
-            case '+':
-                soreNum1 += Integer.parseInt((String) ((Button) view).getText());
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + op);
-        }
+
+        textViewResult.append("\n");
+        op = ((Button) view).getText().charAt(0);
+        //Toast.makeText(this, "" + op, Toast.LENGTH_SHORT).show();
+
+
+        textViewResult.append("" + op + "\n");
 
 
     }
